50cd689eb33db4b3e6c4cb1ab44d30fdebfee224
davideCoderblock <davide@coderblock.com>
2023-11-15 23:12:49 +0100

    (fix lighthouse)

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -161,6 +161,7 @@ const EmailSection = () => {
             </div>
             <button
               type="submit"
+              aria-label="submit"
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               {sending ? "Sending..." : "Send Message"}
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -10,7 +10,7 @@ const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
 
   return (
-    <button onClick={selectTab}>
+    <button  aria-label="onClick" onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
